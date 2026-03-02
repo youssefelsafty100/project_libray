624335791b24df956e23a626056b1d99ba50dcee
youssef_elsafty <youssef.esafty5678@gmail.com>
2026-03-02 23:44:11 +0200

    Add djangorestframework to installed apps

--- MY_PROJECT/MY_PROJECT/settings.py
+++ MY_PROJECT/MY_PROJECT/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app_all',
     'crispy_forms',
-    'crispy_bootstrap4',  
+    'crispy_bootstrap4',
+    'djangorestframework',
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
